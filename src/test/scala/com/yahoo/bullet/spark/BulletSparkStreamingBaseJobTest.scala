3efbe894a631b07642db1dbc6fbbb35cc2823e83
Haibin Xie <xie.haibin@yahoo.com>
2018-05-24 20:21:34 -0700

    debug

--- src/test/scala/com/yahoo/bullet/spark/BulletSparkStreamingBaseJobTest.scala
+++ src/test/scala/com/yahoo/bullet/spark/BulletSparkStreamingBaseJobTest.scala
@@ -7,9 +7,6 @@ package com.yahoo.bullet.spark
 
 import java.io.File
 
-import org.apache.spark.streaming.StreamingContext
-import org.scalatest.BeforeAndAfter
-
 // scalastyle:off
 import scala.collection.JavaConverters._
 // scalastyle:on
@@ -25,23 +22,17 @@ import org.scalatest.time.{Millis, Span}
 import org.scalatest.{FlatSpec, Matchers}
 
 
-class BulletSparkStreamingBaseJobTest extends FlatSpec with Matchers with BeforeAndAfter with Eventually {
+class BulletSparkStreamingBaseJobTest extends FlatSpec with Matchers with Eventually {
   // Override waiting time to 10s since it's a spark streaming with checkpoint.
   override implicit val patienceConfig: PatienceConfig = PatienceConfig(timeout = scaled(Span(10000, Millis)))
 
-  var ssc: StreamingContext = _
-
   behavior of "The bullet spark streaming job"
-
-  after {
-    ssc.stop(stopSparkContext = true, stopGracefully = false)
-  }
 
   it should "run end to end successfully" in {
     System.getProperties.setProperty("spark.master", "local[4]")
     val config = new BulletSparkConfig("src/test/resources/test_config.yaml")
     val job = new BulletSparkStreamingBaseJob()
-    ssc = job.getOrCreateContext(config)
+    val ssc = job.getOrCreateContext(config)
     ssc.start()
 
     val json = makeSimpleAggregationFilterQuery("field", List("fake_field").asJava, Operation.EQUALS, RAW, 1)
@@ -56,6 +47,8 @@ class BulletSparkStreamingBaseJobTest extends FlatSpec with Matchers with Before
         println("sent length:" + CustomPublisher.publisher.sent.length)
       }
       CustomPublisher.publisher.sent.length should equal(1)
+      ssc.sparkContext.stop()
+      ssc.stop(false)
     }
   }
 
@@ -80,11 +73,13 @@ class BulletSparkStreamingBaseJobTest extends FlatSpec with Matchers with Before
     ResultPublisher.clearInstance()
     BulletSparkConfig.clearInstance()
 
-    ssc = job.getOrCreateContext(config)
-    ssc.start()
+    val ssc2 = job.getOrCreateContext(config)
+    ssc2.start()
 
     eventually {
       CustomPublisher.publisher.sent.length should equal(1)
+      ssc2.sparkContext.stop()
+      ssc2.stop(false)
     }
   }
 }
